1453e6d8d30f719a7d8e5c9fff6672b6776d4b3b
Andrés Fulla <andres.fulla@liferay.com>
2020-01-22 21:08:20 +0100

    Step 23

--- src/__tests__/components/Convertert.js
+++ src/__tests__/components/Convertert.js
@@ -1,5 +1,6 @@
 import Converter from '../../components/Converter';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
@@ -20,5 +21,26 @@ describe('Converter', () => {
     expect(getByText(expectedCryptoShortLabel)).toBeInTheDocument();
 
     expect(getByText(`Euros to ${expectedCryptoLabel}`)).toBeInTheDocument();
+  });
+
+  test('converts values inputed at a given rate', async () => {
+    const expectedCryptoLabel = 'Test coin';
+    const expectedCryptoShortLabel = 'TC';
+    const exchangeRate = 0.5;
+
+    const { getByLabelText } = render(
+      <Converter
+        cryptoLabel={expectedCryptoLabel}
+        cryptoShortLabel={expectedCryptoShortLabel}
+        exchangeRate={exchangeRate}
+      />,
+    );
+
+    const eurosInput = getByLabelText('EUR');
+    const testCoinInput = getByLabelText(expectedCryptoShortLabel);
+
+    await userEvent.type(eurosInput, '5');
+
+    expect(testCoinInput.value).toBe('2.5');
   });
 });
